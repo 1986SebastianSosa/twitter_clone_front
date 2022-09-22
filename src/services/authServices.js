2983fece7a22848bb485bec0fea8785b6ead27fd
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-22 20:04:02 -0300

    Update authServices.js

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -14,7 +14,9 @@ export const registerUser = async (values) => {
 export const loginUser = async (values) => {
   const body = values;
   try {
-    const response = await axios.post(`${apiUrl}/auth/login`, body);
+    const response = await axios.post(`${apiUrl}/auth/login`, body, {
+      headers: { "Access-Control-Allow-Origin": "*" },
+    });
     return response;
   } catch (err) {
     return err;
